8e8944588047934e9c4de945420834fe5e0ac6dc
Iain McHugh <46798029+IainMcHugh@users.noreply.github.com>
2024-05-10 08:34:18 +0100

    Awaited schema - add interceptors

--- lib/zaxiod.spec.ts
+++ lib/zaxiod.spec.ts
@@ -1,11 +1,26 @@
-import { describe, test, expect } from "vitest";
+import { vitest, describe, test, expect } from "vitest";
 import { zaxiod } from "./zaxiod";
 
 const MOCK_BASE_URL = "MOCK_BASE_URL";
+const MOCK_TOKEN = "MOCK_TOKEN";
+const mockLogger = vitest.fn();
 
-describe("Axiod", () => {
+describe("Zaxiod", () => {
   test("it works", () => {
-    const gateway = zaxiod({ baseURL: MOCK_BASE_URL });
+    const gateway = zaxiod({
+      baseURL: MOCK_BASE_URL,
+      onReq: (req) => {
+        req.headers = {
+          ...req.headers,
+          Authorization: MOCK_TOKEN,
+        };
+        return req;
+      },
+      onRes: (res) => {
+        mockLogger(res.status);
+        return res;
+      },
+    });
     expect(true).toBeTruthy();
   });
 });

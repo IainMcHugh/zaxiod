2e2cd2d483739eaaa4656261b8cffd777ad166cc
Iain McHugh <46798029+IainMcHugh@users.noreply.github.com>
2024-05-20 08:51:53 +0100

    add biome formatter

--- lib/zaxiod.spec.ts
+++ lib/zaxiod.spec.ts
@@ -7,33 +7,33 @@ const MOCK_TOKEN = "MOCK_TOKEN";
 const mockLogger = vitest.fn();
 
 const mockSchema = z.object({
-  id: z.number(),
+	id: z.number(),
 });
 
 describe("Zaxiod", () => {
-  test("it works", async () => {
-    const gateway = zaxiod({
-      baseURL: MOCK_BASE_URL,
-      onReq: (req) => {
-        req.headers = {
-          ...req.headers,
-          Authorization: MOCK_TOKEN,
-        };
-        return req;
-      },
-      onRes: (res) => {
-        mockLogger(res.status);
-        return res;
-      },
-    });
-    const response = await gateway.get(mockSchema)("/api");
-    if (response.success) {
-      const data = await response.data;
-      console.log(data);
-    } else {
-      const error = response.error.message;
-      console.log(error);
-    }
-    expect(true).toBeTruthy();
-  });
+	test("it works", async () => {
+		const gateway = zaxiod({
+			baseURL: MOCK_BASE_URL,
+			onReq: (req) => {
+				req.headers = {
+					...req.headers,
+					Authorization: MOCK_TOKEN,
+				};
+				return req;
+			},
+			onRes: (res) => {
+				mockLogger(res.status);
+				return res;
+			},
+		});
+		const response = await gateway.get(mockSchema)("/api");
+		if (response.success) {
+			const data = await response.data;
+			console.log(data);
+		} else {
+			const error = response.error.message;
+			console.log(error);
+		}
+		expect(true).toBeTruthy();
+	});
 });
--- lib/zaxiod.ts
+++ lib/zaxiod.ts
@@ -1,13 +1,13 @@
 import type { ZodTypeAny, ZodNever, SafeParseReturnType } from "zod";
-import { z } from "zod";
+import type { z } from "zod";
 
 type Zinfer<Z extends ZodTypeAny = ZodNever> = z.infer<Z>;
 
 export type ZaxiodConfig = {
-  baseURL: string;
-  baseHeaders?: HeadersInit;
-  onReq?(req: RequestInit): RequestInit;
-  onRes?(res: Response): typeof res;
+	baseURL: string;
+	baseHeaders?: HeadersInit;
+	onReq?(req: RequestInit): RequestInit;
+	onRes?(res: Response): typeof res;
 };
 
 const GET = { method: "GET" };
@@ -17,74 +17,74 @@ const PATCH = { method: "PATCH" };
 const DELETE = { method: "DELETE" };
 
 const zaxiod = (baseConfig: ZaxiodConfig) => ({
-  get:
-    <Z extends ZodTypeAny = ZodNever>(schema: Z) =>
-    async (path: string, headers?: HeadersInit) =>
-      await combine<Z>(schema)(baseConfig, path, {
-        ...headers,
-        ...GET,
-      }),
-  post:
-    <Z extends ZodTypeAny = ZodNever>(schema: Z) =>
-    async (path: string, data: Zinfer<Z>, headers?: HeadersInit) =>
-      await combine<Z>(schema)(baseConfig, path, { ...headers, ...POST }, data),
-  put:
-    <Z extends ZodTypeAny = ZodNever>(schema: Z) =>
-    async (path: string, data: Zinfer<Z>, headers?: HeadersInit) =>
-      await combine<Z>(schema)(baseConfig, path, { ...headers, ...PUT }, data),
-  patch:
-    <Z extends ZodTypeAny = ZodNever>(schema: Z) =>
-    async (path: string, data: Partial<Zinfer<Z>>, headers?: HeadersInit) =>
-      await combine<Z>(schema)(
-        baseConfig,
-        path,
-        { ...headers, ...PATCH },
-        data,
-      ),
-  delete:
-    <Z extends ZodTypeAny = ZodNever>(schema: Z) =>
-    async (path: string, headers?: HeadersInit) =>
-      await combine<Z>(schema)(baseConfig, path, {
-        ...headers,
-        ...DELETE,
-      }),
+	get:
+		<Z extends ZodTypeAny = ZodNever>(schema: Z) =>
+		async (path: string, headers?: HeadersInit) =>
+			await combine<Z>(schema)(baseConfig, path, {
+				...headers,
+				...GET,
+			}),
+	post:
+		<Z extends ZodTypeAny = ZodNever>(schema: Z) =>
+		async (path: string, data: Zinfer<Z>, headers?: HeadersInit) =>
+			await combine<Z>(schema)(baseConfig, path, { ...headers, ...POST }, data),
+	put:
+		<Z extends ZodTypeAny = ZodNever>(schema: Z) =>
+		async (path: string, data: Zinfer<Z>, headers?: HeadersInit) =>
+			await combine<Z>(schema)(baseConfig, path, { ...headers, ...PUT }, data),
+	patch:
+		<Z extends ZodTypeAny = ZodNever>(schema: Z) =>
+		async (path: string, data: Partial<Zinfer<Z>>, headers?: HeadersInit) =>
+			await combine<Z>(schema)(
+				baseConfig,
+				path,
+				{ ...headers, ...PATCH },
+				data,
+			),
+	delete:
+		<Z extends ZodTypeAny = ZodNever>(schema: Z) =>
+		async (path: string, headers?: HeadersInit) =>
+			await combine<Z>(schema)(baseConfig, path, {
+				...headers,
+				...DELETE,
+			}),
 });
 
 const combine =
-  <Z extends ZodTypeAny = ZodNever>(schema: Z) =>
-  async (
-    baseConfig: ZaxiodConfig,
-    path: string,
-    headers?: HeadersInit,
-    data?: Zinfer<Z> | Partial<Zinfer<Z>>,
-  ) => {
-    const requestInit: RequestInit = data
-      ? {
-          ...headers,
-          body: JSON.stringify(data),
-        }
-      : { headers };
-    return await fetchWrapper<Z>(baseConfig, schema)(path, requestInit);
-  };
+	<Z extends ZodTypeAny = ZodNever>(schema: Z) =>
+	async (
+		baseConfig: ZaxiodConfig,
+		path: string,
+		headers?: HeadersInit,
+		data?: Zinfer<Z> | Partial<Zinfer<Z>>,
+	) => {
+		const requestInit: RequestInit = data
+			? {
+					...headers,
+					body: JSON.stringify(data),
+				}
+			: { headers };
+		return await fetchWrapper<Z>(baseConfig, schema)(path, requestInit);
+	};
 
 const fetchWrapper =
-  <Z extends ZodTypeAny = ZodNever>(config: ZaxiodConfig, schema: Z) =>
-  async (...args: Parameters<typeof fetch>) => {
-    const request: RequestInit = {
-      ...args[1],
-      headers: {
-        ...config.baseHeaders,
-        ...args[1]?.headers,
-      },
-    };
-    const req: RequestInit = config.onReq ? config.onReq(request) : request;
-    const fetched = await fetch(`${config.baseURL}${args[0]}`, req);
-    const response = config.onRes ? config.onRes(fetched) : fetched;
-    const res = await response.json();
-    return schema.safeParse(res) as SafeParseReturnType<
-      Promise<Zinfer<Z>>,
-      Promise<Zinfer<Z>>
-    >;
-  };
+	<Z extends ZodTypeAny = ZodNever>(config: ZaxiodConfig, schema: Z) =>
+	async (...args: Parameters<typeof fetch>) => {
+		const request: RequestInit = {
+			...args[1],
+			headers: {
+				...config.baseHeaders,
+				...args[1]?.headers,
+			},
+		};
+		const req: RequestInit = config.onReq ? config.onReq(request) : request;
+		const fetched = await fetch(`${config.baseURL}${args[0]}`, req);
+		const response = config.onRes ? config.onRes(fetched) : fetched;
+		const res = await response.json();
+		return schema.safeParse(res) as SafeParseReturnType<
+			Promise<Zinfer<Z>>,
+			Promise<Zinfer<Z>>
+		>;
+	};
 
 export { zaxiod };
